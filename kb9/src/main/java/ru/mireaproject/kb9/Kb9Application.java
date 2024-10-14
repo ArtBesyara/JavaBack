ff1f2b432bbadfa13374d78d015f99880e9d3aa9
Samuramaks <shortwing@list.ru>
2024-10-14 19:47:32 +0300

    init

---
+++ kb9/src/main/java/ru/mireaproject/kb9/Kb9Application.java
@@ -0,0 +1,13 @@
+package ru.mireaproject.kb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kb9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kb9Application.class, args);
+	}
+
+}
---
+++ kb9/src/test/java/ru/mireaproject/kb9/Kb9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mireaproject.kb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kb9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
